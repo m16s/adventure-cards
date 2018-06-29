fc70c46028cb11562be64532a7a8bfce5a05806e
vandemorc <m.strasinskis@gmail.com>
2018-06-29 18:16:33 +0200

    Add empty ng project

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { PlayerComponent } from './components/player/player.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    PlayerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
